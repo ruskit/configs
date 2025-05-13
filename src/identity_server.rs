c31a1ba95aeebc1bf1cc8ea79117d87cd292e54f
ralvescosta <rafael.rac.mg@gmail.com>
2025-05-13 06:47:52 -0300

    feat: Enhance IdentityServerConfigs to support initialization from environment variables

--- src/identity_server.rs
+++ src/identity_server.rs
@@ -25,22 +25,70 @@
 /// ```
 #[derive(Debug, Clone)]
 pub struct IdentityServerConfigs {
+    /// ENV KEY: "IDENTITY_SERVER_URL"
+    ///
     /// Identity Server URL (Default: "")
     pub url: String,
+    /// ENV KEY: "IDENTITY_SERVER_REALM"
+    ///
     /// Identity Application Realm (Default: "")
     ///
     /// In Auth0, Realm is the same as Domain
     pub realm: String,
+    /// ENV KEY: "IDENTITY_SERVER_AUDIENCE"
+    ///
     /// OAuth audience value (Default: "")
     pub audience: String,
+    /// ENV KEY: "IDENTITY_SERVER_ISSUER"
+    ///
     /// OAuth token issuer (Default: "")
     pub issuer: String,
+    /// ENV KEY: "IDENTITY_SERVER_CLIENT_ID"
+    ///
     /// OAuth client ID (Default: "")
     pub client_id: String,
+    /// ENV KEY: "IDENTITY_SERVER_CLIENT_SECRET"
+    ///
     /// OAuth client secret (Default: "")
     pub client_secret: String,
+    /// ENV KEY: "IDENTITY_SERVER_GRANT_TYPE"
+    ///
     /// OAuth grant type (Default: "client_credentials")
     pub grant_type: String,
+}
+
+pub const IDENTITY_SERVER_URL_ENV_KEY: &str = "IDENTITY_SERVER_URL";
+pub const IDENTITY_SERVER_REALM_ENV_KEY: &str = "IDENTITY_SERVER_REALM";
+pub const IDENTITY_SERVER_AUDIENCE_ENV_KEY: &str = "IDENTITY_SERVER_AUDIENCE";
+pub const IDENTITY_SERVER_ISSUER_ENV_KEY: &str = "IDENTITY_SERVER_ISSUER";
+pub const IDENTITY_SERVER_GRANT_TYPE_ENV_KEY: &str = "IDENTITY_SERVER_GRANT_TYPE";
+pub const IDENTITY_SERVER_CLIENT_ID_ENV_KEY: &str = "IDENTITY_SERVER_CLIENT_ID";
+pub const IDENTITY_SERVER_CLIENT_SECRET_ENV_KEY: &str = "IDENTITY_SERVER_CLIENT_SECRET";
+
+impl IdentityServerConfigs {
+    /// Creates a new `IdentityServerConfigs` instance from environment variables.
+    ///
+    /// This method initializes the identity server configuration from environment variables
+    /// for URL, realm, audience, issuer, client ID, client secret, and grant type.
+    ///
+    /// ## Returns
+    ///
+    /// A new `IdentityServerConfigs` from environment variables.
+    pub fn new() -> Self {
+        let mut cfgs = Self::default();
+
+        cfgs.url = std::env::var(IDENTITY_SERVER_URL_ENV_KEY).unwrap_or(cfgs.url);
+        cfgs.realm = std::env::var(IDENTITY_SERVER_REALM_ENV_KEY).unwrap_or(cfgs.realm);
+        cfgs.audience = std::env::var(IDENTITY_SERVER_AUDIENCE_ENV_KEY).unwrap_or(cfgs.audience);
+        cfgs.issuer = std::env::var(IDENTITY_SERVER_ISSUER_ENV_KEY).unwrap_or(cfgs.issuer);
+        cfgs.client_id = std::env::var(IDENTITY_SERVER_CLIENT_ID_ENV_KEY).unwrap_or(cfgs.client_id);
+        cfgs.client_secret =
+            std::env::var(IDENTITY_SERVER_CLIENT_SECRET_ENV_KEY).unwrap_or(cfgs.client_secret);
+        cfgs.grant_type =
+            std::env::var(IDENTITY_SERVER_GRANT_TYPE_ENV_KEY).unwrap_or(cfgs.grant_type);
+
+        cfgs
+    }
 }
 
 impl Default for IdentityServerConfigs {
