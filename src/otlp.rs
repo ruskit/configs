e8d261f2264d1795b875927301c875d01368830e
ralvescosta <rafael.rac.mg@gmail.com>
2025-05-13 06:48:35 -0300

    feat: Add OTLPConfigs struct with environment variable initialization

---
+++ src/otlp.rs
@@ -0,0 +1,89 @@
+use std::time::Duration;
+
+#[derive(Debug, Clone)]
+pub struct OTLPConfigs {
+    /// ENV KEY: "OTLP_EXPORTER_ENDPOINT"
+    ///
+    /// The endpoint for the OTLP service.
+    pub endpoint: String,
+    /// ENV KEY: "OTLP_ACCESS_KEY"
+    ///
+    /// The authentication token for the OTLP service.
+    pub access_key: String,
+    /// ENV KEY: "OTLP_EXPORTER_TIMEOUT"
+    ///
+    /// The timeout duration for the OTLP service.
+    pub exporter_timeout: Duration,
+    /// ENV KEY: "OTLP_EXPORTER_INTERVAL"
+    ///
+    /// The interval duration for the OTLP service.
+    pub exporter_interval: Duration,
+    /// ENV KEY: "OTLP_EXPORTER_RATE_BASE"
+    ///
+    /// The base rate for the OTLP service.
+    pub exporter_rate_base: f64,
+    /// ENV KEY: "OTLP_METRICS_ENABLED"
+    ///
+    /// The flag to enable or disable metrics.
+    pub metrics_enabled: bool,
+    /// ENV KEY: "OTLP_TRACES_ENABLED"
+    ///
+    /// The flag to enable or disable traces.
+    pub traces_enabled: bool,
+}
+
+pub const OTLP_EXPORTER_ENDPOINT_ENV_KEY: &str = "OTLP_EXPORTER_ENDPOINT";
+pub const OTLP_ACCESS_KEY_ENV_KEY: &str = "OTLP_ACCESS_KEY";
+pub const OTLP_EXPORTER_TIMEOUT_ENV_KEY: &str = "OTLP_EXPORTER_TIMEOUT";
+pub const OTLP_EXPORTER_INTERVAL_ENV_KEY: &str = "OTLP_EXPORTER_INTERVAL";
+pub const OTLP_EXPORTER_RATE_BASE_ENV_KEY: &str = "OTLP_EXPORTER_RATE_BASE";
+pub const OTLP_METRICS_ENABLED_ENV_KEY: &str = "OTLP_METRICS_ENABLED";
+pub const OTLP_TRACES_ENABLED_KEY_ENV_KEY: &str = "OTLP_TRACES_ENABLED";
+
+impl OTLPConfigs {
+    /// Creates a new instance of `OTLPConfigs` from environment variable
+    pub fn new() -> Self {
+        let mut cfg = Self::default();
+
+        cfg.endpoint = std::env::var(OTLP_EXPORTER_ENDPOINT_ENV_KEY).unwrap_or(cfg.endpoint);
+        cfg.access_key = std::env::var(OTLP_ACCESS_KEY_ENV_KEY).unwrap_or(cfg.access_key);
+        cfg.exporter_timeout = std::env::var(OTLP_EXPORTER_TIMEOUT_ENV_KEY)
+            .ok()
+            .and_then(|s| s.parse::<u64>().ok())
+            .map(Duration::from_secs)
+            .unwrap_or(cfg.exporter_timeout);
+        cfg.exporter_interval = std::env::var(OTLP_EXPORTER_INTERVAL_ENV_KEY)
+            .ok()
+            .and_then(|s| s.parse::<u64>().ok())
+            .map(Duration::from_secs)
+            .unwrap_or(cfg.exporter_interval);
+        cfg.exporter_rate_base = std::env::var(OTLP_EXPORTER_RATE_BASE_ENV_KEY)
+            .ok()
+            .and_then(|s| s.parse::<f64>().ok())
+            .unwrap_or(cfg.exporter_rate_base);
+        cfg.metrics_enabled = std::env::var(OTLP_METRICS_ENABLED_ENV_KEY)
+            .ok()
+            .and_then(|s| s.parse::<bool>().ok())
+            .unwrap_or(cfg.metrics_enabled);
+        cfg.traces_enabled = std::env::var(OTLP_TRACES_ENABLED_KEY_ENV_KEY)
+            .ok()
+            .and_then(|s| s.parse::<bool>().ok())
+            .unwrap_or(cfg.traces_enabled);
+
+        return cfg;
+    }
+}
+
+impl Default for OTLPConfigs {
+    fn default() -> Self {
+        Self {
+            endpoint: "http://localhost:4317".to_string(),
+            access_key: "token".to_string(),
+            exporter_timeout: Duration::from_secs(60),
+            exporter_interval: Duration::from_secs(60),
+            exporter_rate_base: 0.8,
+            metrics_enabled: false,
+            traces_enabled: false,
+        }
+    }
+}
