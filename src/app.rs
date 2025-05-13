df17fe7d45cf840745da1a351e1dedb168dde9f3
ralvescosta <rafael.rac.mg@gmail.com>
2025-05-13 06:47:30 -0300

    feat: Refactor AppConfigs structure to include environment variable keys and improve default initialization

--- src/app.rs
+++ src/app.rs
@@ -4,7 +4,7 @@
 
 //! Defines the core application configuration settings.
 
-use crate::{Environment, SecretsManagerKind};
+use super::{environment::Environment, secrets::SecretsManagerKind};
 
 /// # AppConfigs
 ///
@@ -23,40 +23,69 @@ use crate::{Environment, SecretsManagerKind};
 /// ```
 #[derive(Debug, Clone)]
 pub struct AppConfigs {
-    ///Default: APP_NAME
+    ///ENV KEY: "APP_NAME"
+    ///
+    ///Default: default-name
     pub name: String,
+    ///ENV KEY: "RUST_ENV"
+    ///
     ///Default: Environment::Local
     pub env: Environment,
+    ///ENV KEY: "SECRET_MANAGER"
+    ///
     ///Default:false
     pub secret_manager: SecretsManagerKind,
+    ///ENV KEY: "SECRET_KEY"
+    ///
     ///Default: context
     pub secret_key: String,
+    ///ENV KEY: "HOST_NAME"
+    ///
     ///Default: 0.0.0.0
     pub host: String,
+    ///ENV KEY: "APP_PORT"
+    ///
     ///Default: 31033
     pub port: u64,
+    ///ENV KEY: "LOG_LEVEL"
+    ///
     ///Default: debug
     pub log_level: String,
+    ///ENV KEY: "ENABLE_EXTERNAL_CREATES_LOGGING"
+    ///
     ///Default: false
     pub enable_external_creates_logging: bool,
 }
 
-impl Default for AppConfigs {
-    fn default() -> Self {
-        Self {
-            name: "APP_NAME".to_owned(),
-            env: Environment::Local,
-            secret_manager: SecretsManagerKind::default(),
-            secret_key: "context".to_owned(),
-            host: "0.0.0.0".to_owned(),
-            port: 31033,
-            log_level: "debug".to_owned(),
-            enable_external_creates_logging: false,
-        }
-    }
-}
+pub const APP_NAME_ENV_KEY: &str = "APP_NAME";
+pub const SECRET_MANAGER_ENV_KEY: &str = "SECRET_MANAGER";
+pub const SECRET_KEY_ENV_KEY: &str = "SECRET_KEY";
+pub const HOST_NAME_ENV_KEY: &str = "HOST_NAME";
+pub const APP_PORT_ENV_KEY: &str = "APP_PORT";
+pub const LOG_LEVEL_ENV_KEY: &str = "LOG_LEVEL";
 
 impl AppConfigs {
+    pub fn new() -> Self {
+        let mut cfg = Self::default();
+
+        cfg.name = std::env::var(APP_NAME_ENV_KEY).unwrap_or(cfg.name);
+        cfg.env = Environment::from_rust_env();
+        cfg.secret_manager = std::env::var(SECRET_MANAGER_ENV_KEY)
+            .unwrap_or("None".into())
+            .as_str()
+            .try_into()
+            .unwrap_or_default();
+        cfg.secret_key = std::env::var(SECRET_KEY_ENV_KEY).unwrap_or(cfg.secret_key);
+        cfg.host = std::env::var(HOST_NAME_ENV_KEY).unwrap_or(cfg.host);
+        cfg.port = std::env::var(APP_PORT_ENV_KEY)
+            .ok()
+            .and_then(|s| s.parse::<u64>().ok())
+            .unwrap_or(cfg.port);
+        cfg.log_level = std::env::var(LOG_LEVEL_ENV_KEY).unwrap_or(cfg.log_level);
+
+        cfg
+    }
+
     /// Returns the formatted address string in the format "host:port".
     ///
     /// This is a convenience method for getting the complete bind address
@@ -67,5 +96,20 @@ impl AppConfigs {
     /// A String containing the formatted address (e.g., "0.0.0.0:31033").
     pub fn app_addr(&self) -> String {
         format!("{}:{}", self.host, self.port)
+    }
+}
+
+impl Default for AppConfigs {
+    fn default() -> Self {
+        Self {
+            name: "default-name".to_owned(),
+            env: Environment::Local,
+            secret_manager: SecretsManagerKind::default(),
+            secret_key: "context".to_owned(),
+            host: "0.0.0.0".to_owned(),
+            port: 31033,
+            log_level: "debug".to_owned(),
+            enable_external_creates_logging: false,
+        }
     }
 }
