c97ce9122ca60fcabd65ccf40b6338e56921532e
ralvescosta <rafael.rac.mg@gmail.com>
2025-05-13 06:48:11 -0300

    feat: Enhance PostgresConfigs to support initialization from environment variables

--- src/postgres.rs
+++ src/postgres.rs
@@ -25,18 +25,32 @@
 /// ```
 #[derive(Debug, Clone)]
 pub struct PostgresConfigs {
+    /// ENV KEY: "POSTGRES_HOST"
+    ///
     /// The PostgreSQL server host (Default: "localhost")
     pub host: String,
+    /// ENV KEY: "POSTGRES_USER"
+    ///
     /// The PostgreSQL username (Default: "")
     pub user: String,
+    /// ENV KEY: "POSTGRES_PASSWORD"
+    ///
     /// The PostgreSQL password (Default: "")
     pub password: String,
+    /// ENV KEY: "POSTGRES_PORT"
+    ///
     /// The PostgreSQL server port (Default: 0)
     pub port: u16,
+    /// ENV KEY : "POSTGRES_DB"
+    ///
     /// The PostgreSQL database name (Default: "")
     pub db: String,
+    /// ENV KEY: "POSTGRES_SSL_MODE"
+    ///
     /// The SSL mode for the connection (Default: PostgresSslMode::Disabled)
     pub ssl_mode: PostgresSslMode,
+    /// ENV KEY: "POSTGRES_CA_PATH"
+    ///
     /// Path to CA certificate for SSL verification (Default: "")
     pub ca_path: String,
 }
@@ -87,6 +101,44 @@ impl From<String> for PostgresSslMode {
         }
 
         Self::Disabled
+    }
+}
+
+pub const POSTGRES_HOST_ENV_KEY: &str = "POSTGRES_HOST";
+pub const POSTGRES_PORT_ENV_KEY: &str = "POSTGRES_PORT";
+pub const POSTGRES_USER_ENV_KEY: &str = "POSTGRES_USER";
+pub const POSTGRES_PASSWORD_ENV_KEY: &str = "POSTGRES_PASSWORD";
+pub const POSTGRES_DB_ENV_KEY: &str = "POSTGRES_DB";
+pub const POSTGRES_SSL_MODE_ENV_KEY: &str = "POSTGRES_SSL_MODE";
+pub const POSTGRES_CA_PATH_ENV_KEY: &str = "POSTGRES_CA_PATH";
+
+impl PostgresConfigs {
+    /// Creates a new `PostgresConfigs` with environment variables.
+    ///
+    /// This method initializes the PostgreSQL configuration with environment variables
+    /// for the host, port, user, password, database name, SSL mode, and CA path.
+    ///
+    /// ## Returns
+    ///
+    /// A new `PostgresConfigs` with environment variables.
+    pub fn new() -> Self {
+        let mut cfgs = Self::default();
+
+        cfgs.host = std::env::var(POSTGRES_HOST_ENV_KEY).unwrap_or(cfgs.host);
+        cfgs.port = std::env::var(POSTGRES_PORT_ENV_KEY)
+            .ok()
+            .and_then(|v| v.parse::<u16>().ok())
+            .unwrap_or(cfgs.port);
+        cfgs.user = std::env::var(POSTGRES_USER_ENV_KEY).unwrap_or(cfgs.user);
+        cfgs.password = std::env::var(POSTGRES_PASSWORD_ENV_KEY).unwrap_or(cfgs.password);
+        cfgs.db = std::env::var(POSTGRES_DB_ENV_KEY).unwrap_or(cfgs.db);
+        cfgs.ssl_mode = std::env::var(POSTGRES_SSL_MODE_ENV_KEY)
+            .ok()
+            .map(|v| PostgresSslMode::from(v))
+            .unwrap_or(cfgs.ssl_mode);
+        cfgs.ca_path = std::env::var(POSTGRES_CA_PATH_ENV_KEY).unwrap_or(cfgs.ca_path);
+
+        cfgs
     }
 }
 
