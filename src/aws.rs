6fb90e3329c318e2fea15c8234a144951742e1e5
ralvescosta <rafael.rac.mg@gmail.com>
2025-05-13 06:47:34 -0300

    feat: Enhance AwsConfigs structure with environment variable keys and initialization method

--- src/aws.rs
+++ src/aws.rs
@@ -24,12 +24,45 @@
 /// ```
 #[derive(Debug, Clone)]
 pub struct AwsConfigs {
+    /// ENV KEY: "AWS_IAM_ACCESS_KEY_ID"
+    ///
     /// AWS access key ID (Default: "local")
     pub access_key_id: Option<String>,
+    /// ENV KEY: "AWS_IAM_SECRET_ACCESS_KEY"
+    ///
     /// AWS secret access key (Default: "local")
     pub secret_access_key: Option<String>,
+    /// ENV KEY:
+    ///
     /// AWS session token (Default: None)
     pub session_token: Option<String>,
+}
+
+pub const AWS_DEFAULT_REGION: &str = "us-east-1";
+pub const AWS_IAM_ACCESS_KEY_ID: &str = "AWS_IAM_ACCESS_KEY_ID";
+pub const AWS_IAM_SECRET_ACCESS_KEY: &str = "AWS_IAM_SECRET_ACCESS_KEY";
+
+impl AwsConfigs {
+    /// Creates a new `AwsConfigs` instance with environments variables.
+    ///
+    /// This method initializes the AWS configuration with environments variables
+    /// for access key ID, secret access key, and session token.
+    ///
+    /// ## Returns
+    ///
+    /// A new `AwsConfigs` with environment variables.
+    pub fn new() -> Self {
+        let mut cfgs = Self::default();
+
+        cfgs.access_key_id = std::env::var(AWS_IAM_ACCESS_KEY_ID)
+            .ok()
+            .or_else(|| std::env::var("AWS_ACCESS_KEY_ID").ok());
+        cfgs.secret_access_key = std::env::var(AWS_IAM_SECRET_ACCESS_KEY)
+            .ok()
+            .or_else(|| std::env::var("AWS_SECRET_ACCESS_KEY").ok());
+
+        cfgs
+    }
 }
 
 impl Default for AwsConfigs {
